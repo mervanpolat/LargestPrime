b03bf2c5d4f12e7f086b5ba6427cd3465e292c28
Mervan Polat <mervanpolat1@icloud.com>
2022-08-17 10:14:11 +0200

    This method calculates the largest prime factor of a given number and returns it.

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 114;
+        primeFactors(number);
+    }
+
+    private static void primeFactors(int number) {
+        System.out.println("Entered value: " + number);
+
+        HashSet<Integer> hashSet=new LinkedHashSet<>();
+        for (int i = 2; i <= number; i++) {
+            while (number%i == 0) {
+                hashSet.add(i);
+                number = number/i;
+            }
+    }
+        int maxSum = 0;
+        int sum = 0;
+        for (Integer integer : hashSet) {
+            System.out.println(integer);
+            sum = sum + integer;
+            if (integer>maxSum) {
+                maxSum = integer;
+            }
+        }
+        System.out.println("Max prime: " + maxSum);
+        System.out.println("Sum = " + sum);
+}
+
+}
